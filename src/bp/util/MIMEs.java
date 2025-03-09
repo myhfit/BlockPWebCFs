12bc977d4a94a40754a62d9ce11200b3719e2328
myhfit <myhfit@126.com>
2025-03-09 21:36:36 +0800

    Monthly update add mime type xml

--- src/bp/util/MIMEs.java
+++ src/bp/util/MIMEs.java
@@ -2,7 +2,7 @@ package bp.util;
 
 public enum MIMEs
 {
-	TEXT("text/plain;charset=utf-8"), HTML_TEXT("text/html"), JSON_APPLICATION("application/json"), JSON_FILE("text/json"), BMP_IMAGE("image/bmp"), PNG_IMAGE("image/png"), JPEG_IMAGE("image/jpeg")
+	TEXT("text/plain;charset=utf-8"), HTML_TEXT("text/html"), XML_TEXT("text/xml"), JSON_APPLICATION("application/json"), JSON_FILE("text/json"), BMP_IMAGE("image/bmp"), PNG_IMAGE("image/png"), JPEG_IMAGE("image/jpeg")
 
 	;
 
