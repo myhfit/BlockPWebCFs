4233e925b870cd1a62f4bdfb828f981b917dcc7a
myhfit <myhfit@126.com>
2026-01-12 22:58:19 +0800

    monthly update update format mime datas

--- src/bp/format/BPFormatHTML.java
+++ src/bp/format/BPFormatHTML.java
@@ -3,6 +3,8 @@ package bp.format;
 public class BPFormatHTML implements BPFormat
 {
 	public final static String FORMAT_HTML = "HTML";
+	public final static String MIME_TEXT_HTML = "text/html";
+	public final static String MIME_APP_HTML = "application/html";
 
 	public String getName()
 	{
@@ -11,6 +13,11 @@ public class BPFormatHTML implements BPFormat
 
 	public String[] getExts()
 	{
-		return new String[] { ".html", ".htm", "text/html" };
+		return new String[] { ".html", ".htm", MIME_TEXT_HTML, MIME_APP_HTML };
+	}
+
+	public String getMIME()
+	{
+		return MIME_TEXT_HTML;
 	}
 }
